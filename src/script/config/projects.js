3cb7752fde3f62940d46a4a4060d6bac23fdf929
Julio-Pineda-Namebot <julionicolas548@gmail.com>
2025-07-13 17:27:16 -0500

    Added topic 1 - Types of leadership

--- src/script/config/projects.js
+++ src/script/config/projects.js
@@ -1,7 +1,7 @@
 export const PROJECTS_CONFIG = {
-  primero: { name: "Primero Tema", url: "/projects/tema-uno" },
-  segundo: { name: "Segundo Tema", url: "/projects/tema-dos" },
-  tercero: { name: "Tercer Tema", url: "/projects/tema-tres" },
+  primero: { name: "Primer tema - Estilos de Liderazgo", url: "/projects/tema-uno" },
+  segundo: { name: "Segundo Tema - ", url: "/projects/tema-dos" },
+  tercero: { name: "Tercer Tema - Habilidades clave para liderar reuniones", url: "/projects/tema-tres" },
 }
 
 export const DEFAULT_BRAND_NAME = "Liderazgo Y Gestión de Equipos"
