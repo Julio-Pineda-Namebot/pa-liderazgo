2a7351b3432a1e9152bfbaae4a6b30a4e5c5f1ce
Julio-Pineda-Namebot <julionicolas548@gmail.com>
2025-07-13 00:49:35 -0500

    Update

--- src/script/config/projects.js
+++ src/script/config/projects.js
@@ -1,7 +1,7 @@
 export const PROJECTS_CONFIG = {
   primero: { name: "Primero Tema", url: "/projects/tema-uno" },
   segundo: { name: "Segundo Tema", url: "/projects/tema-dos" },
-  tercero: { name: "Tercero Tema", url: "/projects/tema-tres" },
+  tercero: { name: "Tercer Tema", url: "/projects/tema-tres" },
 }
 
 export const DEFAULT_BRAND_NAME = "Liderazgo Y Gestión de Equipos"
